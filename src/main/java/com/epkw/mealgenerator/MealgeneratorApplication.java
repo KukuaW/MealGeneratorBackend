1ff20495bebfac700f79d9c371dd99e2ca838cbe
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2022-07-03 00:52:10 +0100

    Initial commit

---
+++ src/main/java/com/epkw/mealgenerator/MealgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.epkw.mealgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epkw/mealgenerator/MealgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epkw.mealgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
